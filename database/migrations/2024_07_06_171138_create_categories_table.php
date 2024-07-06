8b0128185fcfa471dec1185370bc19920cce7d5a
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-07-06 19:18:39 -0300

    feat(categories): alterando campo de exclusão lógica

--- database/migrations/2024_07_06_171138_create_categories_table.php
+++ database/migrations/2024_07_06_171138_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 45);
-            $table->boolean('deleted')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
