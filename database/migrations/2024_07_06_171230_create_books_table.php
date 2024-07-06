59839a89f2f2ee5636dadf2925cb3e5857eda686
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-07-06 15:54:29 -0300

    feat(books): criando tabela do livro

---
+++ database/migrations/2024_07_06_171230_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 70);
+            $table->string('cover')->nullable();
+            $table->text('description');
+            $table->year('release');
+            $table->string('author', 60);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
