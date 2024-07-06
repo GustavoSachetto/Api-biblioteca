032af9ad3d3f24d4fe03674ec0687ea1a460ed87
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-07-06 19:18:03 -0300

    feat(genres): alterando campo de exclusão lógica

--- database/migrations/2024_07_06_170006_create_genres_table.php
+++ database/migrations/2024_07_06_170006_create_genres_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30);
-            $table->boolean('deleted')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
