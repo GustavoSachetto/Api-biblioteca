1e741c5c98735423afae82a00adf4e59bedd673e
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-07-06 15:54:58 -0300

    feat(reservations): criando tabela da reserva do livro

---
+++ database/migrations/2024_07_06_173701_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('observation')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('book_id')->constrained();
+            $table->boolean('deleted')->default(false);
+            $table->dateTime('deadline_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
