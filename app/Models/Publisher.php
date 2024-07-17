57d55b6215e5469fed6e53c73d7adebba62e4c83
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-07-17 19:44:01 -0300

    feat

---
+++ app/Models/Publisher.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Publisher extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'zip_code',
+        'phone',
+    ];
+}
